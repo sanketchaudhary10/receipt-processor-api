ddfa95570c7ba3838d5afc7f54ce0b1e6944a10e
sanketchaudhary10 <67291486+sanketchaudhary10@users.noreply.github.com>
2025-04-23 23:17:05 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, HTTPException
-from uuid import uuid4
-from models import Receipt
-from typing import Dict
+from fastapi import FastAPI, HTTPException, Path
 from fastapi.responses import JSONResponse
+from uuid import uuid4
+from typing import Dict
+
+from models import Receipt
+from processor import calculate_points
 
 app = FastAPI()
 
@@ -19,14 +21,10 @@ def process_receipt(receipt: Receipt):
     receipt_store[receipt_id] = receipt
     return JSONResponse(content={"id": receipt_id})
 
-from fastapi import Path
-from processor import calculate_points
-
 @app.get("/receipts/{receipt_id}/points")
 def get_receipt_points(receipt_id: str = Path(...)):
     receipt = receipt_store.get(receipt_id)
     if not receipt:
         raise HTTPException(status_code=404, detail="No receipt found for that ID.")
-
     points = calculate_points(receipt)
     return {"points": points}
--- processor.py
+++ processor.py
@@ -4,39 +4,36 @@ import re
 from datetime import datetime
 
 def calculate_points(receipt: Receipt) -> int:
-    total = 0
+    points = 0
 
-    rule_a = len(re.findall(r'[a-zA-Z0-9]', receipt.retailer))
-    print("Rule a:", rule_a)
+    # Rule a
+    points += len(re.findall(r'[a-zA-Z0-9]', receipt.retailer))
 
-    rule_b = 50 if receipt.total.endswith(".00") else 0
-    print("Rule b:", rule_b)
+    # Rule b
+    if receipt.total.endswith(".00"):
+        points += 50
 
-    rule_c = 25 if (float(receipt.total) * 100) % 25 == 0 else 0
-    print("Rule c:", rule_c)
+    # Rule c
+    if (float(receipt.total) * 100) % 25 == 0:
+        points += 25
 
-    rule_d = (len(receipt.items) // 2) * 5
-    print("Rule d:", rule_d)
+    # Rule d
+    points += (len(receipt.items) // 2) * 5
 
-    rule_e = 0
+    # Rule e
     for item in receipt.items:
         desc = item.shortDescription.strip()
         if len(desc) % 3 == 0:
-            added_points = math.ceil(float(item.price) * 0.2)
-            rule_e += added_points
-    print("Rule d:", rule_d)
+            points += math.ceil(float(item.price) * 0.2)
 
+    # Rule g
     purchase_date = datetime.strptime(receipt.purchaseDate, "%Y-%m-%d")
-    rule_g = 6 if purchase_date.day % 2 == 1 else 0
-    print("Rule g:", rule_g)
+    if purchase_date.day % 2 == 1:
+        points += 6
 
+    # Rule h
     purchase_time = datetime.strptime(receipt.purchaseTime, "%H:%M")
-    rule_h = 10 if 14 <= purchase_time.hour < 16 else 0
-    print("Rule h:", rule_h)
+    if 14 <= purchase_time.hour < 16:
+        points += 10
 
-    total = rule_a + rule_b + rule_c + rule_d + rule_e + rule_g + rule_h
-    print("Total points calculated:", total)
-
-    return total
-
-
+    return points
